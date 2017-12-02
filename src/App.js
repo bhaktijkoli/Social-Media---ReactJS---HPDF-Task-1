f38ad013bdaea7f678d7d32289d5e5c350852cf2
Bhaktij Koli <bhaktijkoli121@gmail.com>
2017-12-02 12:05:55 +0530

    Added material UI

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Route } from 'react-router-dom';
 import Header from './Components/Header/Header.jsx';
 import Home from './Components/Home/Home.jsx';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import './App.css';
+
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
+      <MuiThemeProvider>
         <Header/>
         <Route exact path="/" component={Home}/>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {RaisedButton} from 'material-ui';
+
 class Header extends Component {
   render() {
     return (
     <div>
-      
+      <RaisedButton label="Default" />
     </div>
     );
   }
