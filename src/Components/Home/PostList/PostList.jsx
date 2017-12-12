74143236a8368600aec1203663fc01ce829c18b7
Bhaktij Koli <bhaktijkoli121@gmail.com>
2017-12-12 06:01:30 +0530

    Added laoder

--- src/Components/Home/PostList/PostList.jsx
+++ src/Components/Home/PostList/PostList.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Post from "./Post"
 
+import Loader from "./../../Loader/Loader"
+
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
@@ -10,16 +12,22 @@ class PostList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       posts: window.data.posts
     }
+  }
+  componentDidMount() {
+    setTimeout(function () {
+      this.setState({loading:false})
+    }.bind(this), 1000);
   }
   render() {
     const listTrends = this.state.posts.map((post) =>
     <Post key={post.name} post={post}/>);
     return (
-      <div>
+      <Loader loading={this.state.loading}>
         {listTrends}
-      </div>
+      </Loader>
     );
   }
 }
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import {CircularProgress} from 'material-ui';
+
+class Loader extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props)
+  }
+  render() {
+    if(this.props.loading == true) {
+      return (
+        <center style={{marginTop:'50px'}}>
+          <CircularProgress size={80} thickness={5} />
+        </center>
+      );
+    }
+    else {
+      return (
+        <div>
+          {this.props.children}
+        </div>
+      );
+    }
+  }
+}
+
+export default Loader;
